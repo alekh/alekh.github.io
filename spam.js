66a8f8af4b96d9511b07f922e13a5b3e024b7e88
Alekh Agarwal <alekhagarwal@gmail.com>
2021-10-18 10:19:56 -0700

    .

--- spam.js
+++ spam.js
@@ -1,4 +1,4 @@
 function spam(address, service) {
-if(service == 1) document.write(address + '@microsoft.com');
+if(service == 1) document.write(address + '@google.com');
 else document.write(address + '@gmail.com');
 }
